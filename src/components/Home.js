4878fde6de80a3074afa3b29e2666f0b12f2eea8
melanie caro <melanie.caro@stackerine.com>
2020-03-03 15:26:08 +0100

    second hook with set interval

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,23 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 export const Counter = () => {
   const [counter, incrementCounter] = useState(0);
+  const [date, setDate] = useState(new Date());
 
   const addToCounter = () => incrementCounter(counter + 1);
+
+  useEffect(() => {
+    const timeInterval = setInterval(() => {
+      setDate(date => new Date());
+    }, 1000);
+    console.log('hello');
+    return () => clearInterval(timeInterval);
+  }, [date]);
 
   return (
     <div>
       <button onClick={addToCounter}>{counter}</button>
+      <div>{date.toLocaleTimeString()}</div>
     </div>
   );
 };
