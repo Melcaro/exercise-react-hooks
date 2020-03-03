034339cbf9c52da731c3b9c0cb27fa0dde150856
melanie caro <melanie.caro@stackerine.com>
2020-03-03 15:45:49 +0100

    third hook with pause button

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,21 +3,28 @@ import React, { useState, useEffect } from 'react';
 export const Counter = () => {
   const [counter, incrementCounter] = useState(0);
   const [date, setDate] = useState(new Date());
+  const [isPaused, setPause] = useState(false);
 
   const addToCounter = () => incrementCounter(counter + 1);
 
+  const setTimer = () => setPause(isPaused => !isPaused);
+
   useEffect(() => {
-    const timeInterval = setInterval(() => {
-      setDate(date => new Date());
-    }, 1000);
-    console.log('hello');
-    return () => clearInterval(timeInterval);
-  }, [date]);
+    if (!isPaused) {
+      const timeInterval = setInterval(() => {
+        setDate(date => new Date());
+      }, 1000);
+      return () => clearInterval(timeInterval);
+    }
+  }, [isPaused]);
 
   return (
     <div>
       <button onClick={addToCounter}>{counter}</button>
       <div>{date.toLocaleTimeString()}</div>
+      <div>
+        <button onClick={setTimer}>Pause</button>
+      </div>
     </div>
   );
 };
